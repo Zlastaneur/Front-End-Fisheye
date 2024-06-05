555cb1138c1249aff2c77ae5c399e02651deb8a4
Stanley <stanley.defer@hotmail.Fr>
2024-06-05 19:54:50 +0200

    💩 WIP photographers page

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,31 @@
-//Mettre le code JavaScript lié à la page photographer.html
+async function getPhotographersById() {
+    const response = await fetch("../../data/photographers.json")
+    const data = await response.json()
+    console.log(data)
+    
+    let id = new URL(window.location.href).searchParams.get("id");
+    console.log(id)
+
+    const reponse = data.photographers.find((f) => f.id == id)
+    console.log(reponse)
+    injectHtml(reponse)
+    return reponse
+} 
+
+const object = getPhotographersById()
+console.log(object)
+console.log(getPhotographersById())
+getPhotographersById()
+injectHtml()
+
+function injectHtml(object){
+    console.log(object)
+    
+    const banner = document.querySelector(".photograph-header")
+    banner.insertAdjacentHTML("afterbegin", `
+        <h1 class="name"></h1>
+        <button class="contact_button" onclick="displayModal()">Contactez-moi</button>
+        
+        
+    `)
+}
--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,14 +1,18 @@
 function photographerTemplate(data) {
-    const { name, portrait, city, country, tagline, price } = data;
+    const { name, portrait, city, country, tagline, price, id } = data
 
-    const picture = `assets/photographers/${portrait}`;
+    const picture = `assets/photographers/${portrait}`
 
     function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
+        const article = document.createElement( 'article' )
+        const a = document.createElement( 'a' )
+        a.setAttribute("href", `photographer.html?id=${id}`)
+        a.setAttribute("alt", name)
+        const img = document.createElement( 'img' )
         img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
+        img.setAttribute("alt", "")
+        const h2 = document.createElement( 'h2' )
+        h2.textContent = name
         const firstP = document.createElement( 'p' )
         firstP.textContent = `${city}, ${country}`
         firstP.classList.add("localisation")
@@ -19,12 +23,13 @@ function photographerTemplate(data) {
         thirdP.textContent = `${price}€/jour`
         thirdP.classList.add("price")
 
-        article.appendChild(img);
-        article.appendChild(h2);
-        article.appendChild(firstP);
-        article.appendChild(secondP);
-        article.appendChild(thirdP);
-        return (article);
+        article.appendChild(a)
+        a.appendChild(img)
+        a.appendChild(h2)
+        article.appendChild(firstP)
+        article.appendChild(secondP)
+        article.appendChild(thirdP)
+        return (article)
     }
-    return { name, picture, getUserCardDOM }
+    return { getUserCardDOM }
 }
